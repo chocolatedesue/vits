80af33c6cf7261f38c5326d4e8122ebaebff8b24
ccds <1356708169@qq.com>
2022-09-08 23:11:13 +0800

    fix mono

--- recipe/sh_script/docker.sh
+++ recipe/sh_script/docker.sh
@@ -5,4 +5,5 @@ docker run -itd \
 --name demo \
 -p 7860:7860   \
 -v  /root/workspace/vits/logs/senbaka44_fix:/mydata \
+-v /home/ubuntu/workspace/downloads/data/model:/test44k \
 ccdesue/vits_demo   /bin/bash

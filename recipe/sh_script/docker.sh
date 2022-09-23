b94c93a7be15c79bd008af3127a8cde713e4276b
ccds <1356708169@qq.com>
2022-09-23 13:23:49 +0800

    fix script

--- recipe/sh_script/docker.sh
+++ recipe/sh_script/docker.sh
@@ -7,3 +7,10 @@ docker run -itd \
 -v  /root/workspace/vits/logs/senbaka44_fix:/mydata \
 -v /home/ubuntu/workspace/downloads/data/model:/test44k \
 ccdesue/vits_demo   /bin/bash
+
+
+docker run -itd --gpus all \
+-v /root/workspace/22k:/root/workspace/22k \
+-v /root/workspace/work:/root/workspace/work \
+--ipc=host \
+ nvcr.io/nvidia/pytorch:22.08-py3
